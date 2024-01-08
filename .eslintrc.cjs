47ee90cde45d96ff4f12df0b0f95004f1b5796fb
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-01-08 15:08:21 +0000

    Add setup files

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  publicDir: "./public",
+  base: "",
+  build: {
+    // Relative to the root
+    outDir: "./build",
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames() {
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
+  plugins: [
+    react({
+      // Use React plugin in all *.jsx and *.tsx files
+      include: "**/*.{jsx,tsx}",
+    }),
+    EnvironmentPlugin("all"),
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
