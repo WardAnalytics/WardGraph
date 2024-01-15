3a869cf2333c5c7a41a935c71c2e26ed99306470
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-01-15 23:08:53 +0000

    Fix bugs, add social media links (#21)
    * Add link and layout
    
    * Update Hotbar.tsx
    
    * Update Graph.tsx
    
    * Update Hotbar.tsx
    
    * Add social media icons
    
    ---------
    
    Co-authored-by: Pedro Rodrigues <44656907+Rodriguespn@users.noreply.github.com>

--- src/components/Graph/LandingPage/LandingPage.tsx
+++ src/components/Graph/LandingPage/LandingPage.tsx
@@ -4,8 +4,13 @@ import { FC } from "react";
 import logo from "../../../assets/ward-logo-blue-full.svg";
 import Searchbar from "./SearchBar";
 
-// TODO - Fill this with real juicy addressess
-const PossibleAddresses: string[] = [];
+const PossibleAddresses: string[] = [
+  "0x19aa5fe80d33a56d56c78e82ea5e50e5d80b4dff",
+  "0x74487eed1e67f4787e8c0570e8d5d168a05254d4",
+  "0xd1381f89b4feea63e9c6bc97dc9fc2b0c96bf12f",
+  "0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be",
+  "0x12d66f87a04a9e220743712ce6d9bb1b5616b8fc",
+];
 
 interface LandingPageProps {
   setSearchedAddress: (address: string) => void;
