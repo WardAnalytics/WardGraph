0059f482c914b31d9134f0b83c288315bf17d94d
Pedro Rodrigues <pedroneverodrigues@gmail.com>
2024-01-15 13:33:57 +0000

    new google analytics event on search address

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { FirebaseApp, initializeApp } from "firebase/app";
 
 const {
   VITE_FIREBASE_API_KEY: apiKey,
@@ -23,7 +23,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app: FirebaseApp = initializeApp(firebaseConfig);
+const analytics: Analytics = getAnalytics(app);
 
-export default { app, analytics };
+export default {
+  app,
+  analytics,
+};
