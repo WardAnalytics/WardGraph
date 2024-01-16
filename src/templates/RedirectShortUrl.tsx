11967749615e30d6e85105895af7dd23115667f7
Pedro Rodrigues <pedroneverodrigues@gmail.com>
2024-01-16 18:17:21 +0000

    changed the layout of the redirect short url page

--- src/templates/RedirectShortUrl.tsx
+++ src/templates/RedirectShortUrl.tsx
@@ -25,13 +25,27 @@ const RedirectShortUrl: FC = () => {
             if (fullUrl) {
                 window.location.href = fullUrl
             } else {
-                console.error("Invalid url")
                 navigate('/')
+                console.error("Invalid url")
             }
         })
     }, [])
 
-    return <div>You are being redirected to the shared graph</div>;
+    return (
+        // Animation of three dots to show that the page is loading
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="text-2xl text-blue-500 font-bold mb-4">
+                Redirecting to the graph...
+            </h1>
+            <div className="animate-pulse">
+                <div className="flex space-x-2">
+                    <div className="w-3 h-3 bg-blue-500 rounded-full"></div>
+                    <div className="w-3 h-3 bg-blue-500 rounded-full"></div>
+                    <div className="w-3 h-3 bg-blue-500 rounded-full"></div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RedirectShortUrl;
