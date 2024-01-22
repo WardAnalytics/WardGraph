48ed2f3c8d7bc7404f59a76cf448006e75779648
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-01-22 19:07:05 +0000

    Update Hotbar.tsx

--- src/components/Graph/Hotbar/Hotbar.tsx
+++ src/components/Graph/Hotbar/Hotbar.tsx
@@ -1,7 +1,6 @@
 import {
   RectangleGroupIcon,
   ShareIcon,
-  MagnifyingGlassPlusIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
 import { FC, useContext, useMemo, useState } from "react";
