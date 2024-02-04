f5af28e2b97543267bdf4089ed040afa67d8a0ae
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-02-04 15:28:11 +0000

    Update Banner.tsx

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <div className="absolute isolate z-50 flex w-full items-center gap-x-6 overflow-hidden bg-gray-50/25 px-6 py-1.5 backdrop-blur-sm sm:px-3.5 sm:before:flex-1">
       {/* Backgrounds */}
-      <div
+      {/* <div
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
         aria-hidden="true"
       >
@@ -27,7 +27,7 @@ export default function Banner() {
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
         />
-      </div>
+      </div> */}
 
       {/* Text */}
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
