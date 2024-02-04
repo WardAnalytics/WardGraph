0fb6b0d2e54af7e08bc617b0fe626c4aa042aa85
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-02-04 01:35:58 +0000

    Fix styling

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -4,44 +4,60 @@ import { AuthContext } from "../Graph/LandingPage/AuthDialog";
 import authService from "../../services/auth/auth.services";
 
 const ForgotPasswordForm: FC = () => {
-    const {
-        onAuthentication,
-        onResetPasswordSuccess,
-        onResetPasswordError,
-        moveToLoginState,
-    } = useContext(AuthContext);
+  const {
+    onAuthentication,
+    onResetPasswordSuccess,
+    onResetPasswordError,
+    moveToLoginState,
+  } = useContext(AuthContext);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        onAuthentication();
+    onAuthentication();
 
-        const email = e.currentTarget.email.value;
-        
-        authService.resetUserPassword(email, onResetPasswordSuccess, onResetPasswordError);
-    }
+    const email = e.currentTarget.email.value;
 
-    return (
-        <>
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" onSubmit={handleSubmit}>
-                    <div>
-                        <AuthInput label="Email" type="email" name="email" id="email" placeholder="name@company.com" required />
-                    </div>
-                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Reset password</button>
-                </form>
-            </div>
-            <p className="mt-10 text-center text-sm text-gray-500 dark:text-white">
-                Remembered your password?{' '}
-                <button
-                    className="font-semibold leading-6 text-blue-500 hover:text-blue-400 dark:text-white dark:hover:text-slate-100"
-                    onClick={moveToLoginState}
-                >
-                    Login
-                </button>
-            </p>
-        </>
-    )
-}
+    authService.resetUserPassword(
+      email,
+      onResetPasswordSuccess,
+      onResetPasswordError,
+    );
+  };
+
+  return (
+    <>
+      <div className="w-full">
+        <form className=" space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <AuthInput
+              label="Email"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-blue-700 dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+          >
+            Reset password
+          </button>
+        </form>
+      </div>
+      <p className="mt-10 text-center text-sm text-gray-500 dark:text-white">
+        Remembered your password?{" "}
+        <button
+          className="font-semibold leading-6 text-blue-500 hover:text-blue-400 dark:text-white dark:hover:text-slate-100"
+          onClick={moveToLoginState}
+        >
+          Login
+        </button>
+      </p>
+    </>
+  );
+};
 
 export default ForgotPasswordForm;
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -5,77 +5,95 @@ import AuthInput from "../common/auth/AuthInput";
 import SignInWithGoogleButton from "../common/auth/SignInWithGoogleButton";
 
 const LoginForm: FC = () => {
-    const {
-        onAuthentication,
-        onLoginSuccess,
-        onLoginError,
-        onGoogleLoginSucess,
-        onGoogleLoginError,
-        moveToSignUpState,
-        moveToForgotPasswordState
-    } = useContext(AuthContext);
+  const {
+    onAuthentication,
+    onLoginSuccess,
+    onLoginError,
+    onGoogleLoginSucess,
+    onGoogleLoginError,
+    moveToSignUpState,
+    moveToForgotPasswordState,
+  } = useContext(AuthContext);
 
-    const handleGoogleSignIn = () => {
-        authService.signUpWithGoogle(onGoogleLoginSucess, onGoogleLoginError);
-    }
+  const handleGoogleSignIn = () => {
+    authService.signUpWithGoogle(onGoogleLoginSucess, onGoogleLoginError);
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        onAuthentication();
+    onAuthentication();
 
-        const email = e.currentTarget.email.value;
-        const password = e.currentTarget.password.value;
+    const email = e.currentTarget.email.value;
+    const password = e.currentTarget.password.value;
 
-        authService.login(email, password, onLoginSuccess, onLoginError);
-    }
+    authService.login(email, password, onLoginSuccess, onLoginError);
+  };
 
-    return (
-        <>
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" onSubmit={handleSubmit} method="POST">
-                    <div>
-                        <AuthInput label="Email" type="email" name="email" id="email" placeholder="name@company.com" autoComplete="email" required />
-                    </div>
+  return (
+    <>
+      <div className="w-full">
+        <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+          <div>
+            <AuthInput
+              label="Email"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="name@company.com"
+              autoComplete="email"
+              required
+            />
+          </div>
 
-                    <div>
-                        <AuthInput label="Password" type="password" name="password" id="password" placeholder="••••••••" autoComplete="current-password" required />
-                        <div className="flex justify-end mt-2 text-sm">
-                            <button
-                                type="button"
-                                className="font-semibold bg-white text-blue-500 hover:text-blue-400 dark:text-white dark:hover:text-slate-100"
-                                onClick={moveToForgotPasswordState}
-                            >
-                                Forgot password?
-                            </button>
-                        </div>
-                    </div>
-
-                    <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-blue-500 hover:bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                        >
-                            Sign in
-                        </button>
-                    </div>
-                </form>
-
-                <SignInWithGoogleButton text={"Sign in with Google"} handleGoogleSignIn={handleGoogleSignIn} />
-
+          <div>
+            <AuthInput
+              label="Password"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              autoComplete="current-password"
+              required
+            />
+            <div className="mt-2 flex justify-end text-sm">
+              <button
+                type="button"
+                className="bg-white font-semibold text-blue-500 hover:text-blue-400 dark:text-white dark:hover:text-slate-100"
+                onClick={moveToForgotPasswordState}
+              >
+                Forgot password?
+              </button>
             </div>
-            <p className="mt-10 text-center text-sm text-gray-500 dark:text-white">
-                Don't have an account?{' '}
-                <button
-                    type="button"
-                    className="font-semibold leading-6 bg-white text-blue-500 hover:text-blue-400 dark:text-white-600 dark:hover:text-slate-100"
-                    onClick={moveToSignUpState}
-                >
-                    Sign up
-                </button>
-            </p>
-        </>
-    )
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <SignInWithGoogleButton
+          text={"Sign in with Google"}
+          handleGoogleSignIn={handleGoogleSignIn}
+        />
+      </div>
+      <p className="mt-10 text-center text-sm text-gray-500 dark:text-white">
+        Don't have an account?{" "}
+        <button
+          type="button"
+          className="dark:text-white-600 bg-white font-semibold leading-6 text-blue-500 hover:text-blue-400 dark:hover:text-slate-100"
+          onClick={moveToSignUpState}
+        >
+          Sign up
+        </button>
+      </p>
+    </>
+  );
 };
 
 export default LoginForm;
